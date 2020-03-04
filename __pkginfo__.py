1db9a685a861434d8263d843024d704e86251fc2
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-04 11:55:58 +0000

    Bump version: 0.1.2 → 0.2.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -27,7 +27,7 @@ copyright   = """
 2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-VERSION = "0.1.2"
+VERSION = "0.2.0"
 
 modname            = "chemistry_tools"
 py_modules		   = None
