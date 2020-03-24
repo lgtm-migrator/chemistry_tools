07b3c550b8a6edddcd806219808177cfa5ad0030
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-24 13:54:47 +0000

    Bump version: 0.2.0 → 0.2.1

--- chemistry_tools/__init__.py
+++ chemistry_tools/__init__.py
@@ -29,7 +29,7 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "2019 Dominic Davis-Foster"
 
 __license__ = "LGPL"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __email__ = "dominic@davis-foster.co.uk"
 
 
