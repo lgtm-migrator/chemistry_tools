50091f4111dfc7360c05d11bbf6ed6c32a64efce
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-29 22:24:16 +0100

    Removed unnecessary importorskip for pandas

--- tests/test_pubchem/test_pandas.py
+++ tests/test_pubchem/test_pandas.py
@@ -8,11 +8,11 @@ Test optional pandas functionality.
 """
 
 # 3rd party
+import pandas
 import pytest
-pandas = pytest.importorskip('pandas')
 
 # this package
-from chemistry_tools.pubchem.compound import compounds_to_frame, Compound
+from chemistry_tools.pubchem.compound import Compound, compounds_to_frame
 from chemistry_tools.pubchem.lookup import get_compounds
 from chemistry_tools.pubchem.properties import get_properties
 
