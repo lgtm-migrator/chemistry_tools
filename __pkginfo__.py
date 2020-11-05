2fc2fb6cfeecf8346cc406c6aa8375c10d82ac58
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-05 08:27:12 +0000

    Updated files with 'repo_helper'.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,22 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"__license__",
-		"__author__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
 		"repo_root",
 		"install_requires",
 		"extras_require",
-		"project_urls",
-
-		"import_name",
 		]
 
 __copyright__ = """
@@ -40,30 +27,23 @@ __copyright__ = """
 """
 
 __version__ = "0.3.1"
-modname = "chemistry_tools"
-pypi_name = "chemistry_tools"
-import_name = "chemistry_tools"
-__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-short_desc = 'Python tools for analysis of chemical compounds.'
-__author__ = author = 'Dominic Davis-Foster'
-author_email = 'dominic@davis-foster.co.uk'
-github_username = "domdfcoding"
-web = github_url = "https://github.com/domdfcoding/chemistry_tools"
+
 repo_root = pathlib.Path(__file__).parent
 install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
-extras_require = {'pubchem': ['cawdrey>=0.1.7', 'mathematical>=0.1.13', 'Pillow>=7.0.0', 'pyparsing>=2.2.0', 'tabulate>=0.8.3'], 'formulae': ['cawdrey>=0.1.7', 'mathematical>=0.1.13', 'pyparsing>=2.2.0', 'tabulate>=0.8.3'], 'plotting': ['matplotlib>=3.0.0'], 'toxnet': ['beautifulsoup4>=4.7.0'], 'all': ['Pillow>=7.0.0', 'beautifulsoup4>=4.7.0', 'cawdrey>=0.1.7', 'mathematical>=0.1.13', 'matplotlib>=3.0.0', 'pyparsing>=2.2.0', 'tabulate>=0.8.3']}
-
-
-
-conda_description = """Python tools for analysis of chemical compounds.
-
-
-Before installing please ensure you have added the following channels: domdfcoding, conda-forge"""
-__all__.append("conda_description")
-
-
-project_urls = {
-		"Documentation": "https://chemistry_tools.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
+extras_require = {
+		'pubchem': [
+				'cawdrey>=0.1.7', 'mathematical>=0.1.13', 'Pillow>=7.0.0', 'pyparsing>=2.2.0', 'tabulate>=0.8.3'
+				],
+		'formulae': ['cawdrey>=0.1.7', 'mathematical>=0.1.13', 'pyparsing>=2.2.0', 'tabulate>=0.8.3'],
+		'plotting': ['matplotlib>=3.0.0'],
+		'toxnet': ['beautifulsoup4>=4.7.0'],
+		'all': [
+				'Pillow>=7.0.0',
+				'beautifulsoup4>=4.7.0',
+				'cawdrey>=0.1.7',
+				'mathematical>=0.1.13',
+				'matplotlib>=3.0.0',
+				'pyparsing>=2.2.0',
+				'tabulate>=0.8.3'
+				]
 		}
